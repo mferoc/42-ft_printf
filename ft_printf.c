ba114d964675a3016af4eeffde0b12d14649cdf4
mferoc <matheusferreiradeocosta@gmail.com>
2020-10-24 13:26:36 -0300

    correct string printing to the end

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: mathferr <mathferr@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/07 16:15:20 by mathferr          #+#    #+#             */
-/*   Updated: 2020/10/24 04:43:49 by mathferr         ###   ########.fr       */
+/*   Updated: 2020/10/24 13:24:12 by mathferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/ft_printf.h"
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
 	va_list ap;
 
@@ -23,16 +23,16 @@ int ft_printf(const char *format, ...)
 		{
 			ft_putchar(*format);
 		}
-		format++;
 		if (*format == '%')
 		{
-				//format++;
-				ft_specifier_manager(*format);
+			format++;
+			ft_specifier_manager(*format);
 		}
+		format++;
 	}
 }
 
-int main(void) {
-	ft_printf("Print a percent sym\t %%\n");
+int	main(void) {
+	ft_printf("Print a percent sym\t%%\n");
 	return (0);
 }
