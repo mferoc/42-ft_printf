1705d5b93e3c99eee3a323b94e02745179a36aab
mferoc <matheusferreiradeocosta@gmail.com>
2020-10-24 04:46:18 -0300

    debug - print % not work

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mathferr <mathferr@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/07 16:15:20 by mathferr          #+#    #+#             */
-/*   Updated: 2020/10/23 05:36:00 by mathferr         ###   ########.fr       */
+/*   Updated: 2020/10/24 04:43:49 by mathferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,15 @@ int ft_printf(const char *format, ...)
 	va_start(ap, format);
 	while (*format)
 	{
-		va_arg(ap, int);
 		if (*format != '%')
 		{
 			ft_putchar(*format);
-			format++;
 		}
-		else
+		format++;
+		if (*format == '%')
 		{
-			if (*format == '%')
-			{
-				format++;
+				//format++;
 				ft_specifier_manager(*format);
-			}
 		}
 	}
 }
--- ft_specifier_manager.c
+++ ft_specifier_manager.c
@@ -6,7 +6,7 @@
 /*   By: mathferr <mathferr@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/23 05:00:40 by mathferr          #+#    #+#             */
-/*   Updated: 2020/10/23 05:34:01 by mathferr         ###   ########.fr       */
+/*   Updated: 2020/10/24 04:45:42 by mathferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,15 @@
 void	ft_specifier_manager(char specifier)
 {
 	if (specifier == 'c')
-		/**/
-	if (specifier == 's')
-		/**/
-	if (specifier == 'd' || specifier == 'i')
-		/**/
-	if (specifier == 'u')
-		/**/
-	if (specifier == 'x' || specifier == 'X')
-		/**/
-	if (specifier == '%')
+		ft_putchar('c');
+	else if (specifier == 's')
+		ft_putchar('x');
+	else if (specifier == 'd' || specifier == 'i')
+		ft_putchar('d');
+	else if (specifier == 'u')
+		ft_putchar('u');
+	else if (specifier == 'x' || specifier == 'X')
+		ft_putchar('x');
+	else if (specifier == '%')
 		ft_print_percent();
-	return (NULL);
 }
